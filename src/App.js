3da375da4efbfffbd70f4ef96ed917d643ce0e48
saklainMostake <saklain15-12003@diu.edu.bd>
2022-09-26 21:53:41 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+const singer = 
+  {name: 'nayan', profession: 'singer'}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='hello'>Welcome</h1>
+        <div className='music'>
+          <p>Name: {singer.name}</p>
+            <p><small>Profession: {singer.profession}</small></p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
